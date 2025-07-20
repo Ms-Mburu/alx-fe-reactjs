9c43747cf3ffa8e8766bc93141a641a7ac9a68cd
Virginiah Mburu <virginiahstanley9@gmail.com>
2025-07-20 18:04:35 +0300

    Add inline styles and Counter component to app

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -1,6 +1,6 @@
 function MainContent() {
   return (
-    <main>
+    <main style={{ backgroundColor: '#f0f0f0', padding: '15px', fontSize: '16px' }}>
       <p>I love to visit New York, Paris, and Tokyo.</p>
     </main>
   );
