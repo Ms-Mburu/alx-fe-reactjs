51df04bd50b7eb2be3f939142ffd269eb451a3be
Virginiah Mburu <virginiahstanley9@gmail.com>
2025-07-20 17:58:13 +0300

    Styled components and added Counter component to alx-react-app-new

---
+++ alx-react-app-new/src/App.jsx
@@ -0,0 +1,21 @@
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+import WelcomeMessage from './components/WelcomeMessage';
+import UserProfile from './components/UserProfile';
+import Counter from './components/Counter';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <MainContent />
+      <WelcomeMessage />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+      <Counter />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
