4683bbd5270ca49266aac46dc3e47bd9eac69eae
Virginiah Mburu <virginiahstanley9@gmail.com>
2025-08-09 22:47:00 +0300

    Add fetchUserData and searchUsersAdvanced to githubService; update Search component

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,6 +1,12 @@
 import axios from 'axios';
 
-export const searchUsersAdvanced = async (query) => {
-  const response = await axios.get(`https://api.github.com/search/users?q=${encodeURIComponent(query)}`);
+const BASE_URL = 'https://api.github.com';
+
+export async function fetchUserData(username) {
+  const response = await axios.get(`${BASE_URL}/users/${username}`);
   return response.data;
-};
+}
+
+export async function searchUsersAdvanced(query) {
+  const response = await axios.get(`${BASE_URL}/search/users?q=${encodeURIComponent(query)}`);
+  return response.data;
